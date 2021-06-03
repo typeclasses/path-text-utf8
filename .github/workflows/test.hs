edeab269a7f134c2beff0b985ba2b7ee87a78e49
Chris Martin <ch.martin@gmail.com>
2021-06-03 02:18:11 -0600

    raise upper bounds

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -12,9 +12,12 @@ main =
                         , "--constraint=text == 1.2.2.0"
                         ]
             "8.4.3"  -> []
-            "8.6.1"  -> []
+            "8.6.1"  -> [ "--constraint=path == 0.6.1" ]
             "8.8.1"  -> []
             "8.10.1" -> [ "--constraint=bytestring == 0.10.12.1"
+                        , "--constraint=path == 0.8.0"
+                        ]
+            "9.0.1"  -> [ "--constraint=bytestring == 0.11.1.0"
                         , "--constraint=path == 0.7.0"
                         , "--constraint=safe-exceptions == 0.1.7.1"
                         , "--constraint=text == 1.2.4.1"
