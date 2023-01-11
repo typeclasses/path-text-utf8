5a6e7615897f5618e33617bab6f7aa1acd58f958
Chris Martin <ch.martin@gmail.com>
2023-01-11 12:44:57 -0700

    haddock cleanup

--- path-text-utf8/src/Path/Text/UTF8.hs
+++ path-text-utf8/src/Path/Text/UTF8.hs
@@ -35,21 +35,21 @@ data ReadError
 
 type WriteError = IOError
 
--- | Read the contents of a UTF-8 encoded text file.
---
--- May throw 'IOError' or 'UnicodeException'. To handle these errors in 'Either'
--- instead, use 'tryReadFile'.
+{-| Read the contents of a UTF-8 encoded text file
+
+May throw 'IOError' or 'UnicodeException'. To handle these errors in 'Either'
+instead, use 'tryReadFile'. -}
 readFile :: Path base Path.File -> IO Text
 readFile path =
   f <$> BS.readFile (Path.toFilePath path)
   where
     f bs = let !text = TextEncoding.decodeUtf8 bs in text
 
--- | Read the contents of a UTF-8 encoded text file.
---
--- Any 'IOError' or 'UnicodeException' that occurs is caught and returned as a
--- 'ReadError' on the 'Left' side of the 'Either'. To throw these exceptions
--- instead, use 'readFile'.
+{-| Read the contents of a UTF-8 encoded text file
+
+Any 'IOError' or 'UnicodeException' that occurs is caught and returned as a
+'ReadError' on the 'Left' side of the 'Either'. To throw these exceptions
+instead, use 'readFile'. -}
 tryReadFile :: Path base Path.File -> IO (Either ReadError Text)
 tryReadFile path =
   f <$> Exception.tryIO (BS.readFile (Path.toFilePath path))
@@ -57,18 +57,18 @@ tryReadFile path =
     f (Left e) = Left (ReadErrorIO e)
     f (Right bs) = first ReadErrorDecode (TextEncoding.decodeUtf8' bs)
 
--- | Write text to a file in a UTF-8 encoding.
---
--- May throw 'IOError'. To handle this error in 'Either' instead, use
--- 'tryWriteFile'.
+{-| Write text to a file in a UTF-8 encoding
+
+May throw 'IOError'. To handle this error in 'Either' instead, use
+'tryWriteFile'. -}
 writeFile :: Path base Path.File -> Text -> IO ()
 writeFile path text =
   BS.writeFile (Path.toFilePath path) (TextEncoding.encodeUtf8 text)
 
--- | Write text to a file in a UTF-8 encoding.
---
--- Any 'IOError' that occurs is caught and returned on the 'Left' side of the
--- 'Either'. To throw the exception instead, use 'writeFile'.
+{-| Write text to a file in a UTF-8 encoding
+
+Any 'IOError' that occurs is caught and returned on the 'Left' side of the
+'Either'. To throw the exception instead, use 'writeFile'. -}
 tryWriteFile :: Path base Path.File -> Text -> IO (Either WriteError ())
 tryWriteFile path text =
   Exception.tryIO (writeFile path text)
