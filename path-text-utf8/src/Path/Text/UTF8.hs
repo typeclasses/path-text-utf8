a30cd84bb7cef6de6a553176d05dfd56be636dfc
Chris Martin <ch.martin@gmail.com>
2023-01-11 12:45:25 -0700

    code cleanup

--- path-text-utf8/src/Path/Text/UTF8.hs
+++ path-text-utf8/src/Path/Text/UTF8.hs
@@ -29,8 +29,8 @@ import qualified Data.Text.Encoding as TextEncoding
 import Path (Path, parseAbsFile, parseRelFile)
 import qualified Path
 
-data ReadError
-  = ReadErrorIO IOError
+data ReadError =
+    ReadErrorIO IOError
   | ReadErrorDecode UnicodeException
 
 type WriteError = IOError
@@ -41,7 +41,7 @@ May throw 'IOError' or 'UnicodeException'. To handle these errors in 'Either'
 instead, use 'tryReadFile'. -}
 readFile :: Path base Path.File -> IO Text
 readFile path =
-  f <$> BS.readFile (Path.toFilePath path)
+    f <$> BS.readFile (Path.toFilePath path)
   where
     f bs = let !text = TextEncoding.decodeUtf8 bs in text
 
@@ -52,7 +52,7 @@ Any 'IOError' or 'UnicodeException' that occurs is caught and returned as a
 instead, use 'readFile'. -}
 tryReadFile :: Path base Path.File -> IO (Either ReadError Text)
 tryReadFile path =
-  f <$> Exception.tryIO (BS.readFile (Path.toFilePath path))
+    f <$> Exception.tryIO (BS.readFile (Path.toFilePath path))
   where
     f (Left e) = Left (ReadErrorIO e)
     f (Right bs) = first ReadErrorDecode (TextEncoding.decodeUtf8' bs)
@@ -63,15 +63,14 @@ May throw 'IOError'. To handle this error in 'Either' instead, use
 'tryWriteFile'. -}
 writeFile :: Path base Path.File -> Text -> IO ()
 writeFile path text =
-  BS.writeFile (Path.toFilePath path) (TextEncoding.encodeUtf8 text)
+    BS.writeFile (Path.toFilePath path) (TextEncoding.encodeUtf8 text)
 
 {-| Write text to a file in a UTF-8 encoding
 
 Any 'IOError' that occurs is caught and returned on the 'Left' side of the
 'Either'. To throw the exception instead, use 'writeFile'. -}
 tryWriteFile :: Path base Path.File -> Text -> IO (Either WriteError ())
-tryWriteFile path text =
-  Exception.tryIO (writeFile path text)
+tryWriteFile path text = Exception.tryIO (writeFile path text)
 
 first :: (a -> a') -> Either a b -> Either a' b
 first f (Left x) = Left (f x)
